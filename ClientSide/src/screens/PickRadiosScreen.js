a1464aa0c6f409a204f523633ea4c923adf9d959
Elad Aharon <eladaharon065@gmail.com>
2025-07-03 14:39:32 +0300

    WebRTC

--- ClientSide/src/screens/PickRadiosScreen.js
+++ ClientSide/src/screens/PickRadiosScreen.js
@@ -200,7 +200,7 @@ const PickRadiosScreen = ({navigation}) => {
                   </Text>
                 </View>
               </TouchableOpacity>
-            );
+            );on
           })}
 
           <TouchableOpacity
